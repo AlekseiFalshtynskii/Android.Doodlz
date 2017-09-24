c03efa8b764cc811816055464e39dfc68742f7d6
AlekseiFalshtynskii <aleksei.falshtynskii@gmail.com>
2017-09-24 23:06:30 +0300

    view classes

---
+++ app/src/main/java/com/example/aleksei/doodlz/DoodleView.java
@@ -0,0 +1,12 @@
+package com.example.aleksei.doodlz;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DoodleView extends View {
+
+    public DoodleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/example/aleksei/doodlz/EraseImageDialogFragment.java
@@ -0,0 +1,4 @@
+package com.example.aleksei.doodlz;
+
+public class EraseImageDialogFragment {
+}
